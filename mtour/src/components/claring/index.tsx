b3b0e1e187cf78f11addb0c2b175f15d3d5b827d
anzp <anzengping@momenta.ai>
2020-05-15 19:43:45 +0800

    add parking mtour

--- mtour/src/components/claring/index.tsx
+++ mtour/src/components/claring/index.tsx
@@ -1,33 +1,49 @@
-import React from "react";
-import "./index.css";
-import ClaButtonComponents from "../clabutton/button";
+import React from 'react'
+import './index.css'
+import ClaButtonComponents from '../clabutton/button'
 const ClaRingComponent: React.FC = () => {
+  const exchangUrlDataCollect = () => {
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'http://ultron.momenta.works/dashboard/'
+    } else {
+      return 'https://msd-test.momenta.works/FilterTask/FilterBagMonitor'
+    }
+  }
+
+  const exchangeUrlStore = () => {
+    console.log(window.location.pathname)
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'https://cla-mpilot-parking.momenta.works/#/list/cla'
+    } else {
+      return 'https://cla-msquare.momenta.works/#/list/cla'
+    }
+  }
+
+  const exchangeUrlTest = () => {
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'https://benchmark.momenta.works/parking/batch/list'
+    } else {
+      return 'https://benchmark.momenta.works/msb/module/fusion/'
+    }
+  }
+
   return (
     <div className="total">
-      {/* <div className="ringInside"></div> */}
+      {/* 数据采集 */}
       <div className="dataCollection btn_all">
-        <ClaButtonComponents
-          url={"https://msd-test.momenta.works/FilterTask/FilterBagMonitor"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangUrlDataCollect()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据回流 */}
       <div className="dataStorage btn_all">
-        <ClaButtonComponents
-          url={"https://milkyway.momenta.works/#/home/dataCollection"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={'https://milkyway.momenta.works/#/home/dataCollection'} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据分析 */}
       <div className="milkyWay btn_all">
-        <ClaButtonComponents
-          url={"https://cla-msquare.momenta.works/#/list/cla"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangeUrlStore()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据标注 */}
       <div className="DataAnotation btn_all">
-        <ClaButtonComponents
-          url={"#/video"}
-          imgurl={"/reverseButton.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={'#/video'} imgurl={'/reverseButton.gif'}></ClaButtonComponents>
       </div>
       {/* <div className="ModleTrainingEng btn_all">
         <ClaButtonComponents
@@ -35,32 +51,18 @@ const ClaRingComponent: React.FC = () => {
           imgurl={"/reverseButton.gif"}
         ></ClaButtonComponents>
       </div> */}
+      {/* 测试验证 */}
       <div className="TestViladation btn_all">
-        <ClaButtonComponents
-          url={"https://benchmark.momenta.works/msb/module/fusion/"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangeUrlTest()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
       <div className="inner">
-        <img
-          className="inner_img"
-          alt="cla_eve_loop_chi"
-          src="/chiinner.png"
-          height="460px"
-          width="460px"
-        ></img>
+        <img className="inner_img" alt="cla_eve_loop_chi" src="/chiinner.png" height="460px" width="460px"></img>
       </div>
       <div className="outer">
-        <img
-          className="outer_img"
-          alt="cla_eve_loop_chi"
-          src="/chi_outer.png"
-          height="620px"
-          width="660px"
-        ></img>
+        <img className="outer_img" alt="cla_eve_loop_chi" src="/chi_outer.png" height="620px" width="660px"></img>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ClaRingComponent;
+export default ClaRingComponent
--- mtour/src/components/claringEng/index.tsx
+++ mtour/src/components/claringEng/index.tsx
@@ -1,33 +1,48 @@
-import React from "react";
-import "./index.css";
-import ClaButtonComponents from "../clabutton/button";
+import React from 'react'
+import './index.css'
+import ClaButtonComponents from '../clabutton/button'
 const ClaRingComponentEng: React.FC = () => {
+  const exchangUrlDataCollect = () => {
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'http://ultron.momenta.works/dashboard/'
+    } else {
+      return 'https://msd-test.momenta.works/FilterTask/FilterBagMonitor'
+    }
+  }
+
+  const exchangeUrlStore = () => {
+    console.log(window.location.pathname)
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'https://cla-mpilot-parking.momenta.works/#/list/cla'
+    } else {
+      return 'https://cla-msquare.momenta.works/#/list/cla'
+    }
+  }
+
+  const exchangeUrlTest = () => {
+    if (window.location.pathname === '/mpilot-parking/') {
+      return 'https://benchmark.momenta.works/parking/batch/list'
+    } else {
+      return 'https://benchmark.momenta.works/msb/module/fusion/'
+    }
+  }
   return (
     <div className="totalEng">
-      {/* <div className="ringInside"></div> */}
+      {/* 数据采集 */}
       <div className="dataCollectionEng btn_all">
-        <ClaButtonComponents
-          url={"https://msd-test.momenta.works/FilterTask/FilterBagMonitor"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangUrlDataCollect()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据回流 */}
       <div className="dataStorageEng btn_all">
-        <ClaButtonComponents
-          url={"https://milkyway.momenta.works/#/home/dataCollection"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={'https://milkyway.momenta.works/#/home/dataCollection'} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据分析 */}
       <div className="milkyWayEng btn_all">
-        <ClaButtonComponents
-          url={"https://cla-msquare.momenta.works/#/list/cla"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangeUrlStore()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
+      {/* 数据标注 */}
       <div className="DataAnotationEng btn_all">
-        <ClaButtonComponents
-          url={"#/video"}
-          imgurl={"/reverseButton.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={'#/video'} imgurl={'/reverseButton.gif'}></ClaButtonComponents>
       </div>
       {/* <div className="ModleTrainingEng btn_all">
         <ClaButtonComponents
@@ -35,32 +50,18 @@ const ClaRingComponentEng: React.FC = () => {
           imgurl={"/reverseButton.gif"}
         ></ClaButtonComponents>
       </div> */}
+      {/* 测试验证 */}
       <div className="TestViladationEng btn_all">
-        <ClaButtonComponents
-          url={"https://benchmark.momenta.works/msb/module/fusion/"}
-          imgurl={"/button.gif"}
-        ></ClaButtonComponents>
+        <ClaButtonComponents url={exchangeUrlTest()} imgurl={'/button.gif'}></ClaButtonComponents>
       </div>
       <div className="innerEng">
-        <img
-          className="inner_imgEng"
-          alt="cla_eve_loop_eng"
-          src="/eng_inner.png"
-          height="460px"
-          width="460px"
-        ></img>
+        <img className="inner_imgEng" alt="cla_eve_loop_eng" src="/eng_inner.png" height="460px" width="460px"></img>
       </div>
       <div className="outerEng">
-        <img
-          className="outer_imgEng"
-          alt="cla_eve_loop_eng"
-          src="/eng_outer.png"
-          height="600px"
-          width="790px"
-        ></img>
+        <img className="outer_imgEng" alt="cla_eve_loop_eng" src="/eng_outer.png" height="600px" width="790px"></img>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ClaRingComponentEng;
+export default ClaRingComponentEng
